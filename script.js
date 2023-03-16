0c1c46762730f4f6668a95c8839ceb41753dbeb3
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-03-16 20:05:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// This the modal...
+const modal = document.querySelector('.modal');
+// Thsi one is the overlay you can say pop-up screen..
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// for selecting multiple sector we use querySelectorAll.
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+  // remove 'remove' the class in the attribute
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+
+
+overlay.addEventListener('click', closeModal)
